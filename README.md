8c2fdf7ea9981f35699beb2bcccf6779d8ec7cb5
Devessier <baptiste@devessier.fr>
2021-03-08 16:05:49 +0100

    build: download git submodules in setup.sh script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # final
 A root repository containing all project repositories as git submodules
+
+## Setup
+To setup everything run `setup.sh` script.
--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
+# Download submodules
+git submodule init
+git submodule update
+
+# Build taskmasterd binary
 cd taskmasterd
 make
 
 cd -
 
+# Install dependencies for taskmastersh
 cd taskmastersh
 npm ci
 
 cd -
 
+# Install dependencies for web ui
 cd web
 pnpm install
